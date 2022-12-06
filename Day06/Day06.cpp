40dd152fead96b3fe1ea49cd4ec9794b31e2678a
Michael Braedley <mike.braedley@gmail.com>
2022-12-06 07:24:15 -0400

    Day 6 complete

---
+++ Day06/Day06.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <set>
+
+int main()
+{
+	std::filesystem::path inputFile("input.txt");
+	std::vector<char> data;
+	std::ifstream inStrm(inputFile);
+
+	std::set<char> possibleHeader;
+	do
+	{
+		char c;
+		inStrm >> c;
+		data.push_back(c);
+		possibleHeader.clear();
+		if (data.size() >= 4)
+		{
+			possibleHeader.insert(data.rbegin(), data.rbegin() + 4);
+		}
+	} while (possibleHeader.size() != 4);
+
+	std::cout << data.size() << "\n";
+
+	//Part 2
+
+	do
+	{
+		char c;
+		inStrm >> c;
+		data.push_back(c);
+		possibleHeader.clear();
+		if (data.size() >= 14)
+		{
+			possibleHeader.insert(data.rbegin(), data.rbegin() + 14);
+		}
+	} while (possibleHeader.size() != 14);
+
+	std::cout << data.size() << "\n";
+}
