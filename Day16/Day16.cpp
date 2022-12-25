87ca4266028994d78e5e6b41ef1cd7fefce4f9d4
Michael Braedley <mike.braedley@gmail.com>
2022-12-25 13:31:26 -0400

    Day 16 complete

--- Day16/Day16.cpp
+++ Day16/Day16.cpp
@@ -12,6 +12,9 @@
 #include <cassert>
 #include <queue>
 #include <unordered_set>
+#include <map>
+#include <set>
+#include <sstream>
 #include <chrono>
 
 using FlowRate = fluent::NamedType<std::int64_t, struct _FlowRate, fluent::Arithmetic>;
@@ -164,20 +167,13 @@ int main()
 
 	using Path = std::tuple<std::shared_ptr<Valve>, std::unordered_set<std::string>, TimeLeft, Score>;
 
-	auto pathCompLess = [](const Path& lhs, const Path& rhs)
-	{
-		const auto& [vl, _l, tl, sl] = lhs;
-		const auto& [vr, _r, tr, sr] = rhs;
-		return sl + vl->GetScore(tl) < sr + vr->GetScore(tr);
-	};
-
-	std::priority_queue<Path, std::vector<Path>, decltype(pathCompLess)> searchQueue(pathCompLess);
+	std::queue<Path> searchQueue;
 	searchQueue.push({ valves["AA"], {}, TimeLeft(30), Score(0) });
 	Score bestScore{ 0 };
 
 	while (!searchQueue.empty())
 	{
-		auto [val, visited, timeLeft, score] = searchQueue.top();
+		auto [val, visited, timeLeft, score] = searchQueue.front();
 		searchQueue.pop();
 
 		visited.emplace(val->GetName());
@@ -205,4 +201,94 @@ int main()
 
 	std::cout << bestScore.get() << "\n";
 	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(part1 - preprocessing) << "\n";
+
+	//Part 2
+	std::queue<std::pair<Path, Path>> search2Queue;
+	search2Queue.push(std::make_pair<Path, Path>({ valves["AA"], {}, TimeLeft(26), Score(0) }, { valves["AA"], {}, TimeLeft(26), Score(0) }));
+	
+	std::map<std::string, Score> stateScores;
+
+	auto stateToString = [](const std::unordered_set<std::string>& state) -> std::string
+	{
+		std::set<std::string> orderedState(state.begin(), state.end());
+		std::stringstream sstrm;
+		for (auto& s : orderedState)
+		{
+			sstrm << s << ",";
+		}
+
+		return sstrm.str();
+	};
+
+	while (!search2Queue.empty())
+	{
+		auto [p1, p2] = search2Queue.front();
+		search2Queue.pop();
+
+		auto [val1, visited1, timeLeft1, score1] = p1;
+		auto [val2, visited2, timeLeft2, score2] = p2;
+
+		score1 += val1->GetScore(timeLeft1);
+		score2 += val2->GetScore(timeLeft2);
+
+		std::unordered_set<std::string> state;
+		state.insert(visited1.begin(), visited1.end());
+		state.insert(visited2.begin(), visited2.end());
+		state.insert(val1->GetName());
+		state.insert(val2->GetName());
+
+		std::string stateStr = stateToString(state);
+		if (!stateScores.contains(stateStr) || stateScores[stateStr] < score1 + score2)
+		{
+			stateScores[stateStr] = score1 + score2;
+		}
+		else
+		{
+			continue;	//No point continuing on this branch, it won't produce a better score
+		}
+
+		if (!visited1.contains(val1->GetName()))
+		{
+			visited1.emplace(val1->GetName());
+
+			auto otherValves1 = distances.equal_range(val1->GetName());
+			for (auto iter = otherValves1.first; iter != otherValves1.second; iter++)
+			{
+				std::string otherName = std::get<std::string>(iter->second);
+				TimeLeft distTo = std::get<TimeLeft>(iter->second);
+
+				if (!visited1.contains(otherName) && !visited2.contains(otherName) && otherName != val2->GetName() && distTo < timeLeft1)
+				{
+					search2Queue.emplace(std::make_tuple(valves[otherName], visited1, timeLeft1 - distTo, score1), p2);
+				}
+			}
+		}
+
+		if (!visited2.contains(val2->GetName()))
+		{
+			visited2.emplace(val2->GetName());
+			auto otherValves2 = distances.equal_range(val2->GetName());
+			for (auto iter = otherValves2.first; iter != otherValves2.second; iter++)
+			{
+				std::string otherName = std::get<std::string>(iter->second);
+				TimeLeft distTo = std::get<TimeLeft>(iter->second);
+
+				if (!visited1.contains(otherName) && !visited2.contains(otherName) && otherName != val1->GetName() && distTo < timeLeft2)
+				{
+					search2Queue.emplace(p1, std::make_tuple(valves[otherName], visited2, timeLeft2 - distTo, score2));
+				}
+			}
+		}
+	}
+
+	bestScore.get() = 0;
+	for (auto& [state, score] : stateScores)
+	{
+		if (score > bestScore)
+		{
+			bestScore = score;
+		}
+	}
+
+	std::cout << bestScore.get() << "\n";
 }
